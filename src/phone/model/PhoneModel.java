ae2f6bc7fe506de1c1b0fe86eed290fe4fad1244
Ian Erickson <19ierickson@gmail.com>
2018-11-14 12:22:01 -0700

    added setters and getters

--- src/phone/model/PhoneModel.java
+++ src/phone/model/PhoneModel.java
@@ -2,5 +2,39 @@ package phone.model;
 
 public class PhoneModel
 {
-
+	private String type;
+	private double screenSize;
+	private boolean isAndroid;
+	
+	public PhoneModel()
+	{
+		
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+	public double getScreenSize()
+	{
+		return screenSize;
+	}
+	public boolean getIsAndroid()
+	{
+		return isAndroid;
+	}
+	
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+	public void setScreenSize(double screenSize)
+	{
+		this.screenSize = screenSize;
+	}
+	public void setIsAndroid(boolean isAndroid)
+	{
+		this.isAndroid = isAndroid;
+	}
+	
 }

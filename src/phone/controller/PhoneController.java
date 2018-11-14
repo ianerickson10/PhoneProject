9e738a50d8081022ce13c695b27bd47cc5a2187d
Ian Erickson <19ierickson@gmail.com>
2018-11-14 13:10:00 -0700

    added Valid int and double

--- src/phone/controller/PhoneController.java
+++ src/phone/controller/PhoneController.java
@@ -1,9 +1,52 @@
 package phone.controller;
 
+import javax.swing.JOptionPane;
+
 public class PhoneController
 {
 	public void start()
 	{
+		setVariables();
+	}
+	
+	public void setVariables()
+	{
 		
+	}
+	
+	
+	
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+				
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch (NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You need to type in a whole number");
+		}
+		
+		return isValid;
+	}
+	
+	public boolean validDouble(String mightBeDouble)
+	{
+		Boolean isValid = false;
+		
+		try
+		{
+			Double.parseDouble(mightBeDouble);
+			isValid = true;
+		}
+		catch (NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "Type in a decimal");
+		}
+		
+		return isValid;
 	}
 }
